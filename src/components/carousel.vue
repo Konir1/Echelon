52110b59888fadd2b080073941b3eb16307cb4d3
Chase Murtaugh <Chasemurtaugh@gmail.com>
2022-08-03 22:40:00 -0400

    Actually moves caro

---
+++ src/components/carousel.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="image">
+    <div class="q-pa-lg" style="width: 80%">
+      <q-carousel
+        arrows
+        animated
+        control-color="primary"
+        v-model="slide"
+        height="400px"
+      >
+        <q-carousel-slide
+          name="first"
+          img-src="https://cdn.quasar.dev/img/mountains.jpg"
+        >
+          <div class="absolute-bottom custom-caption">
+            <div class="text-h2">First stop</div>
+            <div class="text-subtitle1">Mountains</div>
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide
+          name="second"
+          img-src="https://cdn.quasar.dev/img/parallax1.jpg"
+        >
+          <div class="absolute-bottom custom-caption">
+            <div class="text-h2">Second stop</div>
+            <div class="text-subtitle1">Famous City</div>
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide
+          name="third"
+          img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+        >
+          <div class="absolute-bottom custom-caption">
+            <div class="text-h2">Third stop</div>
+            <div class="text-subtitle1">Famous Bridge</div>
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+    </div>
+    <div class="textbox">
+      <q-card class="my-card bg-primary text-white">
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+
+        <q-separator dark />
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.custom-caption
+  text-align: center
+  padding: 12px
+  color: white
+  background-color: rgba(0, 0, 0, .3)
+.image
+  display: flex
+  align-items: center
+.textbox
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px
+  margin-right: 25px
+.caption
+  margin-left: 10px
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "carouselWithCard",
+  setup() {
+    return {
+      slide: ref("first"),
+      lorem:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    };
+  },
+});
+</script>
